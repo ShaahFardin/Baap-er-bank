de18c45785a9f1f9eef7228c163d6b0b1188149f
shaahfardin <fardinshaah@gmail.com>
2022-08-17 00:32:30 +0600

    second-commit

--- js/deposit.js
+++ js/deposit.js
@@ -1,29 +1,51 @@
-// add event listener to the deposit button
+// // add event listener to the deposit button
+// document
+//   .getElementById("deposit-button")
+//   .addEventListener("click", function () {
+//     ///   step 2 : get the deposit amount
+
+//     const depositField = document.getElementById("deposit-amount");
+//     const newDepositAmount = parseFloat(depositField.value);
+//     // console.log(depositAmount);
+
+//     // step 3 : get the current deposit amount
+//     const depositTotalElement = document.getElementById("deposit-total");
+//     const previousDepositTotal = parseFloat(depositTotalElement.innerText);
+//     // step 4 : add numbers to set the total deposit
+//     const currentDepositTotal = previousDepositTotal + newDepositAmount;
+//     depositTotalElement.innerText = currentDepositTotal;
+
+//     // step 5 : update the balance section
+
+//     const previouseBalanceElement = document.getElementById("previous-balance");
+//     const previouseBalanceTotalString = previouseBalanceElement.innerText;
+//     const previousBalance = parseFloat(previouseBalanceTotalString);
+
+//     // step 6
+//     const currentBalance = previousBalance + newDepositAmount;
+//     previouseBalanceElement.innerText = currentBalance;
+//     // step 7 : clear the input field
+//     depositField.value = "";
+//   });
+
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
-    ///   step 2 : get the deposit amount
+    const enteredDeposit = document.getElementById("deposit-amount");
+    const enteredDepositAmount = parseFloat(enteredDeposit.value);
 
-    const depositField = document.getElementById("deposit-amount");
-    const newDepositAmount = parseFloat(depositField.value);
-    // console.log(depositAmount);
+    const previousDeposit = document.getElementById("deposit-total");
+    const previousDepositBalance = parseFloat(previousDeposit.innerText);
+    // console.log(typeof previousDepositBalance);
 
-    // step 3 : get the current deposit amount
-    const depositTotalElement = document.getElementById("deposit-total");
-    const previousDepositTotal = parseFloat(depositTotalElement.innerText);
-    // step 4 : add numbers to set the total deposit
-    const currentDepositTotal = previousDepositTotal + newDepositAmount;
-    depositTotalElement.innerText = currentDepositTotal;
-
-    // step 5 : update the balance section
+    const newDepositBalance = previousDepositBalance + enteredDepositAmount;
+    previousDeposit.innerText = newDepositBalance;
 
     const previouseBalanceElement = document.getElementById("previous-balance");
-    const previouseBalanceTotalString = previouseBalanceElement.innerText;
-    const previousBalance = parseFloat(previouseBalanceTotalString);
+    const previousBalance = parseFloat(previouseBalanceElement.innerText);
 
-    // step 6
-    const currentBalance = previousBalance + newDepositAmount;
-    previouseBalanceElement.innerText = currentBalance;
-    // step 7 : clear the input field
-    depositField.value = "";
+    const latestBalance = previousBalance + enteredDepositAmount;
+    previouseBalanceElement.innerText = latestBalance;
+
+    enteredDeposit.value = "";
   });
